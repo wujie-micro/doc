b3047675679c91eb93394b8c81e0975dd968ecb3
xu zhidan <xuzhidan@xudeMacBook-Pro.local>
2022-08-27 12:47:23 +0800

    feat: demo修改

---
+++ assets/api_setupApp.md.52abf6f7.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as p}from"./app.a1fdbfe1.js";const i=JSON.parse('{"title":"setupApp","description":"","frontmatter":{},"headers":[],"relativePath":"api/setupApp.md"}'),l={name:"api/setupApp.md"},o=p("",4),e=[o];function c(t,r,D,y,C,A){return a(),n("div",null,e)}const d=s(l,[["render",c]]);export{i as __pageData,d as default};
